8851cf0ddec7a32d786a881377055a71f4f8d938
Tyler Cross <tyler@cross.solutions>
2025-02-21 11:16:30 -0500

    initial commit

---
+++ scripts/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+readonly script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+install_os_dependencies() {
+    sudo dnf install python3 pipx -y
+}
+
+install_poetry() {
+    pipx install poetry
+}
+
+install_project_dependencies() {
+    pushd "${script_dir}" &> /dev/null
+    poetry install --no-root
+    pushd "../ansible" &> /dev/null
+    poetry run ansible-galaxy collection install -r collections/requirements.yaml --force
+    popd &> /dev/null
+    popd &> /dev/null
+}
+
+install_os_dependencies
+install_poetry
+install_project_dependencies
