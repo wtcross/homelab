9557eb1be36881e60e1ea68eb9e128530bde2426
Tyler Cross <tyler@cross.solutions>
2025-03-20 15:47:11 -0400

    Initial commit

---
+++ scripts/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+readonly script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+readonly project_dir=$( dirname "${script_dir}" )
+
+pushd "${project_dir}" &> /dev/null
+
+sudo dnf install -y uv
+uv python install
+uv venv --allow-existing
+uv sync
+
+
+pushd ansible &> /dev/null
+
+uv run ansible-galaxy collection install -r collections/requirements.yaml --force
+
+popd &> /dev/null
+popd &> /dev/null
+
+# TODO: set up vaultwarden CLI for use with ansible lookup plugin for bitwarden
